eb3154bfb9a5ad24997ec9edb5674fbdae6affae
Gustavo Sandri <gustavo.p.sandri@gmail.com>
2019-10-04 16:36:13 -0300

    Object "stock": added function to compute correlation

--- include/stock.h
+++ include/stock.h
@@ -5,21 +5,24 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
 class stock
 {
-    public:
-        vector<double> values;
+public:
+    vector<double> values;
 
-        stock();
-        virtual ~stock();
-        bool read(const char* filename);
+    stock();
+    virtual ~stock();
+    bool read(const char* filename);
+    double corr(stock &that);
+    double operator*(stock &that);
 
-    protected:
+protected:
 
-    private:
+private:
 };
 
 #endif // STOCK_H
--- src/stock.cpp
+++ src/stock.cpp
@@ -22,7 +22,7 @@ bool stock::read(const char *filename)
     size_t pos[8];
     int i;
 
-    pos[0] = -1;
+    pos[0] = static_cast<size_t>(-1);
 
     getline (fid,line);
 
@@ -43,4 +43,45 @@ bool stock::read(const char *filename)
 
     fid.close();
     return false;
+}
+
+double stock::corr(stock &that)
+{
+    size_t N = this->values.size();
+    if( N>that.values.size() )
+        N = that.values.size();
+
+    vector<double>::iterator v1 = this->values.begin() + static_cast<long>(this->values.size()-N);
+    vector<double>::iterator v2 = that.values.begin()  + static_cast<long>(that.values.size()-N);
+    vector<double>::iterator vend = this->values.end();
+
+    double S12 = 0, S11 = 0, S22 = 0;
+    double m1 = 0, m2 = 0;
+
+    while( v1<vend )
+    {
+        m1 += *(v1++);
+        m2 += *(v2++);
+    }
+    m1 /= N;
+    m2 /= N;
+
+    v1 = this->values.begin() + static_cast<long>(this->values.size()-N);
+    v2 = that.values.begin()  + static_cast<long>(that.values.size()-N);
+    while( v1<vend )
+    {
+        double d1 = *(v1++)-m1;
+        double d2 = *(v2++)-m2;
+
+        S12 += d1*d2;
+        S11 += d1*d1;
+        S22 += d2*d2;
+    }
+
+    return S12/sqrt(S11*S22);
+}
+
+double stock::operator*(stock &that)
+{
+    return this->corr(that);
 }
